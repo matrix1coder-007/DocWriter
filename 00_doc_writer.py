981f9f35262eb8b178cc97ecc4240f5cd4218d9c
Ishita Katyal <70653653+matrix1coder-007@users.noreply.github.com>
2024-09-02 05:43:37 +0530

    Add files via upload

---
+++ 00_doc_writer.py
@@ -0,0 +1,51 @@
+"""
+Author: Ishita Katyal
+Date created: 06/25/2023
+Date modified: 06/25/2023
+Topic: Document Writer
+Description:
+    Initial features:
+        1. Save a document.
+        2. Edit a document.
+        3. Format the text script. --> Color/Font-style/Font-syntax.
+            -- Bold/Italics/Underline/Superscript/Subscript
+            -- Text color/Highlight color
+        4. Cut/Copy/Paste a document.
+        5. Find/replace a word.
+        6. Insert into a document.
+            -- Header
+            -- Footer
+            -- Page number
+            -- Page orientation
+            -- Image
+            -- Link/hyperlink
+            -- Comments and suggestions (--> Open comments section--history)
+        7. Align/indent syntax
+        8. Bullets and numbering
+        9. Last edit/created date history
+        10. Print a document.
+        11. Modes: CRUD principles-->
+            Create, Read, Update, Delete && Review (Comments & suggestions section) -- can access
+            -- Writer : C, R, U, D, Review,
+            -- Viewer : R,
+            -- Editor : R, U, Review,
+            -- Reviewer : R, Review,
+        12. Input formats:
+            -- Speech-to-text
+            -- Text/File
+    Advanced features:
+        1. Undo/Redo the last action.
+        2. Insert into a document.
+            -- Table and Table borders
+            -- Chart
+            -- Special characters
+            -- Equations
+        3. Capitalization: Lower(), Upper(), Title()
+        4. Translation
+        5. Help section
+        6. Training section
+        7. Update section
+        8. Help docs improve? (User end-point)
+        9. Privacy policy/Terms of service
+        10. 
+"""
